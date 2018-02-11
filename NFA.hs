ba24652adf84089bb418fcc5e246996b652fa6cd
higuoxing@gmail.com <higuoxing@outlook.com>
2018-02-11 23:20:28 +0800

    init

---
+++ NFA.hs
@@ -0,0 +1,9 @@
+-- FA (S, Map, s0, F, Sigma)
+-- S     : Set of states
+-- Map   : Transition functions (Moves)
+-- s0    : Initial state
+-- F     : Set of acceptable state
+data NFA a = NFA (Set s) (Set (Move s) s (Set s))
+  deriving (Show, Eq)
+
+
