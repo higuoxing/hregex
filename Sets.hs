9a122e33f78e4f5b2a7078b2cd3b44c38cccfc27
higuoxing@gmail.com <higuoxing@outlook.com>
2018-02-19 10:36:03 +0800

    Sets

--- Sets.hs
+++ Sets.hs
@@ -19,10 +19,11 @@ module Sets (
 import Data.List hiding ( union )
 
 data Set a = SetI [a]
+  deriving Eq
 
 -- empty
 empty  :: Set a
-empty  = SetI []
+empty         = SetI []
 
 -- sing
 sing   :: a -> Set a
@@ -49,24 +50,24 @@ uni (x:xs) (y:ys)
   | otherwise = y : uni (x:xs) ys
 
 -- inter 
-inter :: Ord a => Set a -> Set a -> Set a
+inter  :: Ord a => Set a -> Set a -> Set a
 inter (SetI xs) (SetI ys) = SetI (int xs ys)
 
 -- int
-int :: Ord a => [a] -> [a] -> [a]
-int [] ys = []
-int xs [] = []
+int    :: Ord a => [a] -> [a] -> [a]
+int [] ys     = []
+int xs []     = []
 int (x:xs) (y:ys)
   | x <  y    = int xs (y:ys)
   | x == y    = x : int xs ys
   | otherwise = int (x:xs) ys
 
 -- diff
-diff :: Ord a => Set a -> Set a -> Set a
+diff   :: Ord a => Set a -> Set a -> Set a
 diff (SetI xs) (SetI ys) = SetI (dif xs ys)
 
 -- dif
-dif :: Ord a => [a] -> [a] -> [a]
+dif    :: Ord a => [a] -> [a] -> [a]
 dif [] ys     = []
 dif xs []     = []
 dif (x:xs) (y:ys)
@@ -79,7 +80,7 @@ subSet :: Ord a => Set a -> Set a -> Bool
 subSet (SetI xs) (SetI ys) = subS xs ys
 
 -- subS
-subS :: Ord a => [a] -> [a] -> Bool
+subS   :: Ord a => [a] -> [a] -> Bool
 subS [] ys    = True
 subS xs []    = False
 subS (x:xs) (y:ys)
@@ -88,4 +89,47 @@ subS (x:xs) (y:ys)
   | otherwise = subS (x:xs) ys
 
 -- eqSet
+eqSet  :: Eq a => Set a -> Set a -> Bool
+eqSet (SetI xs) (SetI ys) = xs == ys
 
+-- makeSet   
+makeSet :: Ord a => [a] -> Set a
+makeSet = SetI . remDups . sort
+  where
+    remDups []  = []
+    remDups [x] = [x]
+    remDups (x:x':xs)
+      | x < x'    = x : remDups (x':xs)
+      | otherwise = remDups (x':xs)
+
+-- mapSet    
+mapSet :: Ord b => (a -> b) -> Set a -> Set b
+mapSet f (SetI xs) = makeSet (map f xs)
+
+-- filterSet 
+filterSet :: (a -> Bool) -> Set a -> Set a
+filterSet p (SetI xs) = SetI (filter p xs)
+
+-- foldSet   
+foldSet :: (a -> a -> a) -> a -> Set a -> a
+foldSet f x (SetI xs) = (foldr f x xs)
+
+-- showSet   
+showSet :: Show a => Set a -> IO ()
+showSet (SetI xs) = putStr $ concat (map ((++ "\n") . show) xs)
+
+-- card      
+card :: Set a -> Int
+card (SetI xs) = length xs
+
+-- flatten
+flatten :: Set a -> [a]
+flatten (SetI xs) = xs
+
+-- setLimit  
+setLimit :: Eq a => (Set a -> Set a) -> Set a -> Set a
+setLimit f s
+  | s == next = s
+  | otherwise = setLimit f next
+    where
+      next = f s
