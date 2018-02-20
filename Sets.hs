471f8aba1efdadb4016fd13a4ecdc24e5a942bd5
higuoxing@gmail.com <higuoxing@outlook.com>
2018-02-20 11:40:44 +0800

    modified Sets.hs

--- Sets.hs
+++ Sets.hs
@@ -1,24 +1,24 @@
 module Sets (
     Set                   , -- 
     empty                 , --          Set a
-    sing                  , --          a -> Set a
-    memSet                , -- Ord a  => Set a -> a -> Bool
+    single                , --          a -> Set a
+    memberOf              , -- Ord a  => a -> Set a -> Bool
     union, inter, diff    , -- Ord a  => Set a -> Set a -> Set a
-    eqSet                 , -- Eq  a  => Set a -> Set a -> Bool
     subSet                , -- Ord a  => Set a -> Set a -> Bool
     makeSet               , -- Ord a  => [a]   -> Set a
     mapSet                , -- Ord a  => (a -> b) -> Set a -> Set b
     filterSet             , --           (a -> Bool) -> Set a -> a
-    foldSet               , --           (a -> a -> a) -> a -> Set a -> a
+    foldrSet              , --           (a -> a -> a) -> a -> Set a -> a
+    foldlSet              , --           (a -> a -> a) -> a -> Set a -> a
     showSet               , -- Show a => Set a -> String
-    card                  , --           Set a -> Int
+    len                   , --           Set a -> Int
     flatten               , --           Set a -> [a]
     setLimit                -- Eq  a  => (Set a -> Set a) -> Set a -> Set a
     ) where
 
 import Data.List hiding ( union )
 
-data Set a = SetI [a]
+data Set a = Set [a]
   deriving Eq
 
 instance (Show a) => Show (Set a) where
@@ -26,78 +26,74 @@ instance (Show a) => Show (Set a) where
 
 -- empty
 empty  :: Set a
-empty         = SetI []
+empty         = Set []
 
 -- sing
-sing   :: a -> Set a
-sing x        = SetI [x]
+single :: a -> Set a
+single x      = Set [x]
 
--- memSet (member of)
-memSet :: Ord a => Set a -> a -> Bool
-memSet (SetI []) y = False
-memSet (SetI (x:xs)) y
-  | x <  y    = memSet (SetI xs) y
-  | x == y    = True
-  | otherwise = False
+-- memberOf (member of)
+memberOf :: Ord a => a -> Set a -> Bool
+memberOf _ (Set [])= False
+memberOf x (Set (y:ys))
+  | x > y = memberOf x (Set ys)
+  | x < y = False
+  | otherwise = True
 
 -- union
 union  :: Ord a => Set a -> Set a -> Set a
-union (SetI xs) (SetI ys) = SetI (uni xs ys)
+union (Set xs) (Set ys) = Set (union' xs ys)
 
-uni    :: Ord a => [a] -> [a] -> [a]
-uni [] ys     = ys
-uni xs []     = xs
-uni (x:xs) (y:ys)
-  | x <  y    = x : uni xs (y:ys)
-  | x == y    = x : uni xs ys
-  | otherwise = y : uni (x:xs) ys
+union'    :: Ord a => [a] -> [a] -> [a]
+union' [] ys     = ys
+union' xs []     = xs
+union' (x:xs) (y:ys)
+  | x <  y    = x : union' xs (y:ys)
+  | x == y    = x : union' xs ys
+  | otherwise = y : union' (x:xs) ys
 
 -- inter 
 inter  :: Ord a => Set a -> Set a -> Set a
-inter (SetI xs) (SetI ys) = SetI (int xs ys)
+inter (Set xs) (Set ys) = Set (inter' xs ys)
 
 -- int
-int    :: Ord a => [a] -> [a] -> [a]
-int [] ys     = []
-int xs []     = []
-int (x:xs) (y:ys)
-  | x <  y    = int xs (y:ys)
-  | x == y    = x : int xs ys
-  | otherwise = int (x:xs) ys
+inter'   :: Ord a => [a] -> [a] -> [a]
+inter' [] ys  = []
+inter' xs []  = []
+inter' (x:xs) (y:ys)
+  | x <  y    = inter' xs (y:ys)
+  | x == y    = x : inter' xs ys
+  | otherwise = inter' (x:xs) ys
 
 -- diff
 diff   :: Ord a => Set a -> Set a -> Set a
-diff (SetI xs) (SetI ys) = SetI (dif xs ys)
+diff (Set xs) (Set ys) = Set (diff' xs ys)
 
 -- dif
-dif    :: Ord a => [a] -> [a] -> [a]
-dif [] ys     = []
-dif xs []     = []
-dif (x:xs) (y:ys)
-  | x <  y    = x : dif xs (y:ys)
-  | x == y    = dif xs ys
-  | otherwise = y : dif (x:xs) ys
+diff'    :: Ord a => [a] -> [a] -> [a]
+diff' [] ys   = []
+diff' xs []   = xs
+diff' (x:xs) (y:ys)
+  | x <  y    = x : diff' xs (y:ys)
+  | x == y    = diff' xs ys
+  | otherwise = y : diff' (x:xs) ys
 
 -- subSet
 subSet :: Ord a => Set a -> Set a -> Bool
-subSet (SetI xs) (SetI ys) = subS xs ys
+subSet (Set xs) (Set ys) = subSet' xs ys
 
--- subS
-subS   :: Ord a => [a] -> [a] -> Bool
-subS [] ys    = True
-subS xs []    = False
-subS (x:xs) (y:ys)
+-- subSet'
+subSet'   :: Ord a => [a] -> [a] -> Bool
+subSet' [] ys = True
+subSet' xs [] = False
+subSet' (x:xs) (y:ys)
   | x <  y    = False
-  | x == y    = subS xs ys
-  | otherwise = subS (x:xs) ys
-
--- eqSet
-eqSet  :: Eq a => Set a -> Set a -> Bool
-eqSet (SetI xs) (SetI ys) = xs == ys
+  | x == y    = subSet' xs ys
+  | otherwise = subSet' (x:xs) ys
 
 -- makeSet   
 makeSet :: Ord a => [a] -> Set a
-makeSet = SetI . remDups . sort
+makeSet = Set . remDups . sort
   where
     remDups []  = []
     remDups [x] = [x]
@@ -107,27 +103,31 @@ makeSet = SetI . remDups . sort
 
 -- mapSet    
 mapSet :: Ord b => (a -> b) -> Set a -> Set b
-mapSet f (SetI xs) = makeSet (map f xs)
+mapSet f (Set xs) = makeSet (map f xs)
 
 -- filterSet 
 filterSet :: (a -> Bool) -> Set a -> Set a
-filterSet p (SetI xs) = SetI (filter p xs)
+filterSet p (Set xs) = Set (filter p xs)
 
--- foldSet   
-foldSet :: (a -> a -> a) -> a -> Set a -> a
-foldSet f x (SetI xs) = (foldr f x xs)
+-- foldrSet   
+foldrSet :: (a -> a -> a) -> a -> Set a -> a
+foldrSet f x (Set xs) = foldr f x xs
+
+-- foldlSet
+foldlSet :: (a -> a -> a) -> a -> Set a -> a
+foldlSet f x (Set xs) = foldl f x xs
 
 -- showSet   
 showSet :: Show a => Set a -> String
-showSet (SetI xs) = concat (map ((++ "\n") . show) xs)
+showSet (Set xs) = concat (map ((++ "\n") . show) xs)
 
--- card      
-card :: Set a -> Int
-card (SetI xs) = length xs
+-- length      
+len :: Set a -> Int
+len (Set xs) = length xs
 
 -- flatten
 flatten :: Set a -> [a]
-flatten (SetI xs) = xs
+flatten (Set xs) = xs
 
 -- setLimit  
 setLimit :: Eq a => (Set a -> Set a) -> Set a -> Set a
