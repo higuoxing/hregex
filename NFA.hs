8094680e6043cc136bab1442a7e850d7b445470f
higuoxing@gmail.com <higuoxing@outlook.com>
2018-02-20 13:12:27 +0800

    NFA

--- NFA.hs
+++ NFA.hs
@@ -1,18 +1,28 @@
--- FA (S, Map, s0, F, Sigma)
--- S     : Set of states
--- Map   : Transition functions (Moves)
--- s0    : Initial state
--- F     : Set of acceptable states
+{-# LANGUAGE GADTs #-}
 
 import Sets
 
-data NFA a = NFA (Set a)         -- Set of States
-                 (Set (Move a))  -- Transition Functions
-                 a               -- Initial State
-                 (Set a)         -- Set of Acceptable States
-  deriving (Show, Eq)
+-- FA (S, Map, s0, F, Sigma)
+data NFA state where
+  NFA :: (Eq state, Show state) => (Set state)          -- S     : Set of states
+                                -> (Set (Trans state))  -- Tran  : Transition functions (Moves)
+                                -> state                -- s0    : Initial state
+                                -> (Set state)          -- F     : Set of acceptable states
+                                -> NFA state
 
-type Edge = Char
+instance Show state => Show (NFA state) where
+  show = showNFA
 
-data Move a = Move a a Edge      -- Edge (Char)
-  deriving (Eq, Ord, Show)
+data Trans state = Edge state Char state  -- edge between states
+                 | Epsilon state state
+  deriving (Ord, Eq, Show)
+
+showNFA :: (Show state) => NFA state -> String
+showNFA (NFA ss ts s es) = "states:          \n" ++ 
+                           show ss  ++ 
+                           "edges :          \n" ++
+                           show ts  ++
+                           "initial state:   \n" ++
+                           show s   ++      "\n" ++
+                           "acceptable states\n" ++
+                           show es
